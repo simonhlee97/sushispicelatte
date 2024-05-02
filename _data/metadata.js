4e9fd1e90f83f873d6fe021c7945ce8dffc0ae89
Simon Lee <simonhlee97@hotmail.com>
2024-05-02 09:00:29 -0400

    my second commit

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Jinjja Hoosier",
+	url: "https://simonhlee97.github.io/blog",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Jinjja, in Korean, means real. Hoosier is just a person from Indiana.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Simon",
+		email: "psyminrhee79@icloud.com",
+		url: "https://example.com/"
 	}
 }
